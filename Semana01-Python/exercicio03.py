a0f87c4529c376dcce0d1d9bfcfe5ee7a14d5fd6
Kyska Harrington <129115993+kyska22@users.noreply.github.com>
2024-11-17 18:08:07 -0300

    Create exercicio03.py
    thirt exercice

---
+++ Semana01-Python/exercicio03.py
@@ -0,0 +1,10 @@
+#Area do circulo
+#Neste exercicio, se va a receber o valor do radio do circulo
+#A saida sera o calculo da area
+
+r = int(input("Digite o raio: "))
+p = 3.14159
+
+area = p*r*r
+
+print(area)
