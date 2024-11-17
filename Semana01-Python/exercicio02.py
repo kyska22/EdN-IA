3f2ce37303d4bf636a03c31e017be23853531c62
Kyska Harrington <129115993+kyska22@users.noreply.github.com>
2024-11-17 18:00:32 -0300

    Create exercicio02.py
    second exercice

---
+++ Semana01-Python/exercicio02.py
@@ -0,0 +1,9 @@
+#Este exercicio toma dois valores interos e os soma
+#Imprime a mensagem "X =" o valor da soma
+
+a = int(input("Digite a: "))
+b = int(input("Digite b: "))
+
+x = a + b
+
+print("O resultado é X = ", x)
