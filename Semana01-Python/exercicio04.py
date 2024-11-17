74d60b2f64e81ca9f0c4a618cc093206d1a343b7
Kyska Harrington <129115993+kyska22@users.noreply.github.com>
2024-11-17 18:12:58 -0300

    Create exercicio04.py
    fourth exercice

---
+++ Semana01-Python/exercicio04.py
@@ -0,0 +1,10 @@
+#Produto de dois numeros
+#O usuario vai ingresar dois numeros
+#A saida vai ser o produto dos numeros ingresados
+
+a = int(input("Digite a: "))
+b = int(input("Digite b: "))
+
+x = a * b
+
+print("O resultado é X = ", x)
