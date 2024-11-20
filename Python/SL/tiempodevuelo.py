d72a6d01866a5e1bc81c439d5c1ad98ccb2d1b1b
Kyska Harrington <129115993+kyska22@users.noreply.github.com>
2024-11-20 11:51:59 -0300

    Create tiempodevuelo.py
    first from sololearn

---
+++ Python/SL/tiempodevuelo.py
@@ -0,0 +1,6 @@
+"""
+Necesitas calcular el tiempo de vuelo de un próximo viaje. 
+Vas a volar de Los Ángeles a Sydney, cubriendo una distancia de 7425 millas,
+el avión vuela a una velocidad media de 550 millas por hora.
+"""
+print("El tiempo de vuelo en horas es:", 7425/550)
