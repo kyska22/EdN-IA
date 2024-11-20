754fab9df9a44146961b00bf769b2fd2932b7bc1
Kyska Harrington <129115993+kyska22@users.noreply.github.com>
2024-11-20 11:53:29 -0300

    Create cadenas.py
    second from sololearn

---
+++ Python/SL/cadenas.py
@@ -0,0 +1,18 @@
+"""
+Hay que hacer un programa para una tabla de clasificación.
+El programa tiene que generar los números del 1 al 9, cada uno en una línea separada, seguida de un punto:
+1.
+2.
+3.
+...
+"""
+
+print ("""1.
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.""")
