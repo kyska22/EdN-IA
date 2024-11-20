0dd0141662ff9a446158e78287575ee5b377c717
Kyska Harrington <129115993+kyska22@users.noreply.github.com>
2024-11-20 12:05:04 -0300

    Update ifsentense.py
    elif

--- Python/SL/ifsentense.py
+++ Python/SL/ifsentense.py
@@ -5,3 +5,15 @@ if num > 5:
     print("Bigger than 5")
     if num <=47:
         print("Between 5 and 47")
+#################################################################
+# Para anidar varias sentencias if, podemos usar elif
+
+num = 3
+if num == 1:
+    print("One")
+elif num == 2:
+    print("Two")
+elif num == 3: 
+    print("Three")
+else: 
+    print("Something else")
